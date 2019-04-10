9d372bddcef810803ef0de1ec3810c693d5c228b
fkq.kevin <519754821@qq.com>
2019-04-10 18:40:12 +0800

    增加伪原创工具

---
+++ expandWords/BaiduWordExpand/expandByBaiduMobileRecomandWords_test.go
@@ -0,0 +1,16 @@
+package BaiduWordExpand
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestExpandBaiduRecommendWords(t *testing.T) {
+	keywords, err := ExpandBaiduRecommendWords("勾花网")
+	if err != nil {
+		panic(err)
+	}
+	for _, k := range keywords {
+		fmt.Println(k)
+	}
+}
